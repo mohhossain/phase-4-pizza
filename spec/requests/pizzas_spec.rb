e69f98595a29f42c8ba0baf0a72796c923e4dbc7
ihollander <ianholla@gmail.com>
2021-05-29 07:31:51 -0400

    Initial commit

---
+++ spec/requests/pizzas_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Pizzas", type: :request do
+  before do
+    Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+    Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+  end
+
+  describe "GET /pizzas" do
+    it 'returns an array of all pizzas' do
+      get '/pizzas'
+
+      expect(response.body).to include_json([
+        {
+          id: a_kind_of(Integer),
+          name: "Cheese",
+          ingredients: "Dough, Tomato Sauce, Cheese"
+        },
+        {
+          id: a_kind_of(Integer),
+          name: "Pepperoni",
+          ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni"
+        }
+      ])
+    end
+  end
+
+end
